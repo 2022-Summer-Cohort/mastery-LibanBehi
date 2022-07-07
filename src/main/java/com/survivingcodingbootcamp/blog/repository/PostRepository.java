ffcef645af40a5d9c5332d2d9d8e86c48dda4f5b
LibanBehi <Libanbehi22@gmail.com>
2022-07-07 14:35:43 -0400

    finished

--- src/main/java/com/survivingcodingbootcamp/blog/repository/PostRepository.java
+++ src/main/java/com/survivingcodingbootcamp/blog/repository/PostRepository.java
@@ -1,7 +1,15 @@
 package com.survivingcodingbootcamp.blog.repository;
 
+import com.survivingcodingbootcamp.blog.model.Hashtag;
 import com.survivingcodingbootcamp.blog.model.Post;
+import org.springframework.data.jpa.domain.AbstractPersistable;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.persistence.metamodel.SingularAttribute;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+
 public interface PostRepository extends CrudRepository<Post, Long> {
+
 }
